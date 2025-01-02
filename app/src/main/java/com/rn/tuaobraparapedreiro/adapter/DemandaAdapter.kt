6f5bec8adc11fc6c12a029ad7564bc17ebe5d05c
Rian-Noronha <riannoronha011@gmail.com>
2025-01-02 14:41:15 -0300

    [refactor] formatação demanda

--- app/src/main/java/com/rn/tuaobraparapedreiro/adapter/DemandaAdapter.kt
+++ app/src/main/java/com/rn/tuaobraparapedreiro/adapter/DemandaAdapter.kt
@@ -44,8 +44,25 @@ class DemandaAdapter(
         @RequiresApi(Build.VERSION_CODES.O)
         fun bind(demanda: Demanda) {
             binding.txtTrabalhoASerFeito.text = demanda.trabalhoSerFeito
-            binding.txtHorarioPublicacao.text = demanda.dataPublicacao
+            binding.txtHorarioPublicacao.text = formatarData(demanda.dataPublicacao)
             binding.txtLocalizacao.text = demanda.endereco.cep
         }
+
+
+        fun formatarData(data: String): String {
+            val partesDataHora = data.split("T")
+            val dataParte = partesDataHora[0]
+            val horaParte = partesDataHora[1].split(":")
+            val anoMesDia = dataParte.split("-")
+            val horas = horaParte[0].toInt() - 3
+            val minutos = horaParte[1].toInt()
+            val horaAjustada = if (horas < 0) 24 + horas else horas
+            val dia = anoMesDia[2]
+            val mes = anoMesDia[1]
+            val ano = anoMesDia[0]
+            val horaFormatada = "${horaAjustada}h${if (minutos > 0) "${minutos}min" else ""}"
+            return "Às $horaFormatada, $dia/$mes/$ano"
+        }
+
     }
 }
