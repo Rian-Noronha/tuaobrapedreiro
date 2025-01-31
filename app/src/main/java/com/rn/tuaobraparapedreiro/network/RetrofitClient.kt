9633b4bb6d4509be050a5410574b9f4d121a6321
Rian-Noronha <riannoronha011@gmail.com>
2025-01-31 17:42:43 -0300

    refactor(network) alteração de ip

--- app/src/main/java/com/rn/tuaobraparapedreiro/network/RetrofitClient.kt
+++ app/src/main/java/com/rn/tuaobraparapedreiro/network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.3.127:8080/api/"
+    private const val BASE_URL = "http://10.0.0.100:8080/api/"
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
